3a748b491bad44a6c3766a5c9e81af012f03657d
abdulmahl <mdu_abdul@icloud.com>
2026-09-08 12:32:43 +0200

    .

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -6,4 +6,16 @@ hamButton.addEventListener("click", () => {
   menuItems.classList.toggle("show");
 });
 
+const url =
+  "https://github.com/abdulmahl/wdd231/blob/64a430d30c89d6b03e945fec7a54f1beff33c58d/chamber/data/members.json";
 
+const members = document.querySelector("#members");
+
+const fetchMemberData = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log(data);
+};
+
+fetchMemberData();
