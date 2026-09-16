4981010e8affc2ec768853696f2195dcaf7ee1c3
abdulmahl <mdu_abdul@icloud.com>
2026-09-16 09:00:20 +0200

    .

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,17 +1,17 @@
 const fetchWeather = async () => {
-  let lat = -25.70;
+  let lat = -25.72;
   let lon = 28.38;
-  let cnt = 3;
   let API_key = "291547be3b03e0f848a07cbbe427449e";
-  let url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=${cnt}&appid=${API_key}`;
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`;
 
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
+  const forecast = document.querySelector("#forecast-container");
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  forecast.textContent = data.weather[0].description;
+
+  console.log(data.weather[0].description);
 };
 
 fetchWeather();
